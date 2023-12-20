f937e1d8980b5a1317e959d26b655daacd72b593
riverios <victor@mydae.org>
2023-12-20 15:55:03 -0500

    this is my python file that explains variables

---
+++ 1sttry.py
@@ -0,0 +1,11 @@
+name = "funny"
+time = "night"
+place = "nhv"
+
+print( name )
+print( time )
+print( place )
+
+PIZZA_TIME = 2.48
+
+print( PIZZA_TIME)
---
+++ variables.py
@@ -0,0 +1,9 @@
+age = 12
+name = "victor"
+other = "rios"
+GAS_PRICE = 3.99
+
+print ( age )
+print ( name )
+print ( other )
+print ( GAS_PRICE )
